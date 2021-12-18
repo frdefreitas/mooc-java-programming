62e4fde0fa2bc4019c077580cb010dfa5da73eca
frdefreitas <77462253+frdefreitas@users.noreply.github.com>
2021-12-18 08:17:31 -0300

    Add files via upload

---
+++ Part4/part04-Part04_10.Song/src/main/java/Song.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author Flavio
+ */
+public class Song {
+    private String name;
+    private int length;
+    
+    public Song(String name, int length) {
+        this.name = name;
+        this.length = length;
+        
+    }
+    public String name() {
+        return this.name;
+    }
+    public int length() {
+        return this.length;
+    }
+    
+}
---
+++ Part4/part04-Part04_14.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author Flavio
+ */
+public class Multiplier {
+    private int number;
+    
+    public Multiplier(int number) {
+    this.number = number;    
+    }
+    public int multiply(int number) {
+        return number * this.number;
+    }
+    
+    
+}
